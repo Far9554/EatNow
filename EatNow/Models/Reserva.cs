e75efea237f84a61397790bf22135b320b33b160
Far9554 <marioleoncarrizo@gmail.com>
2023-12-01 17:22:26 +0100

    Creacion de los Modelos

---
+++ EatNow/Models/Reserva.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatNow.Models
+{
+    public class Reserva
+    {
+        [Required]
+        public int IdReserva { get; set; }
+
+        [Required]
+        public DateTime Inicio { get; set; }
+
+        [Required]
+        public DateTime Fin {  get; set; }
+
+        [Required]
+        public int RIdCliente { get; set; }
+
+        public int RIdEstadoReserva { get; set; }
+
+        [Required]
+        public int RIdCasilla { get; set; }
+    }
+}
---
+++ EatNow/Models/Restaurante.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EatNow.Models
+{
+    public class Restaurante
+    {
+        [Required]
+        public int IdRestaurante { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Direccion { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string Telefono { get; set; }
+
+        [StringLength(200)]
+        public string? Web { get; set; }
+
+        [StringLength(200)]
+        public string? Descripcion { get; set; }
+
+        [Required]
+        public string HoraApertura { get; set; }
+
+        [Required]
+        public string HoraCierre { get; set; }
+    }
+}
