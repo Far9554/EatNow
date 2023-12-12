8d4e4b21884354a114861d66ad0c718f42babb26
izjob <izian.job@gmail.com>
2023-12-12 16:07:05 +0100

    navbar

--- EatNow/Program.cs
+++ EatNow/Program.cs
@@ -28,7 +28,7 @@ namespace EatNow
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Login}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
