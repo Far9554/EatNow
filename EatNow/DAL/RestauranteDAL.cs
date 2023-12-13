4755594addf2c481c6211dd50a92bf13af0daab7
Far9554 <marioleoncarrizo@gmail.com>
2023-12-13 17:21:09 +0100

    Formulario Añadir/Eliminar Foto Restaurante

--- EatNow/DAL/RestauranteDAL.cs
+++ EatNow/DAL/RestauranteDAL.cs
@@ -113,5 +113,42 @@ namespace EatNow.DAL
                 }
             }
         }
+
+        public List<Restaurante> GetRestaurantsByFilter()
+        {
+            List<Restaurante> restaurants = new List<Restaurante>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT IdRestaurante, Nombre, Direccion, Telefono, Web, Descripcion, " +
+                               "CONVERT(VARCHAR(5), HoraApertura, 108) AS HoraApertura, " +
+                               "CONVERT(VARCHAR(5), HoraCierre, 108) AS HoraCierre FROM Restaurante";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Restaurante restaurant = new Restaurante
+                            {
+                                IdRestaurante = int.Parse(reader["IdRestaurante"].ToString()),
+                                Nombre = reader["Nombre"].ToString(),
+                                Direccion = reader["Direccion"].ToString(),
+                                Telefono = reader["Telefono"].ToString(),
+                                Web = (reader["Web"] != DBNull.Value) ? reader["Web"].ToString() : null,
+                                Descripcion = (reader["Descripcion"] != DBNull.Value) ? reader["Descripcion"].ToString() : null,
+                                HoraApertura = reader["HoraApertura"].ToString(),
+                                HoraCierre = reader["HoraCierre"].ToString()
+                            };
+                            restaurants.Add(restaurant);
+                        }
+                    }
+                }
+            }
+
+            return restaurants;
+        }
     }
 }
